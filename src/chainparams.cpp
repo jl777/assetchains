4c116d3e7fc132c370803fa87b0eea8e3f8c9fd7
jl777 <jameslee777@yahoo.com>
2016-10-06 20:54:41 -0300

    test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -223,7 +223,7 @@ void *chainparams_commandline(void *ptr)
         fprintf(stderr,"couldnt find nonce, abort\n");
         exit(-1);
     }
-    consensus->recalc_genesis(nonce);
+    //consensus->recalc_genesis(nonce);
     fprintf(stderr,"%u: hash %s <= target %s\n",nonce,hash.ToString().c_str(),tmp.ToString().c_str());
     consensus->consensus.hashGenesisBlock = hash;
     fprintf(stderr,"%s: port.%u magic.%08x %u nonce.%u time.%u nbits.%08x %u coins\n",ASSETCHAINS_SYMBOL,ASSETCHAINS_PORT,ASSETCHAINS_MAGIC,ASSETCHAINS_MAGIC,nonce,ASSETCHAINS_TIMESTAMP,GENESIS_NBITS,(uint32_t)ASSETCHAINS_SUPPLY);
