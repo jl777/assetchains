1206172c72ce184ceff3670e3f4d8b81b3aa759c
jl777 <jameslee777@yahoo.com>
2016-10-06 17:55:17 -0300

    test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -183,7 +183,7 @@ public:
             tmp = genesis.GetHash();
             if ( UintToArith256(tmp) > bnTarget )
             {
-                fprintf(stderr,"%u: hash %llx > %llx target\n",nonce,(long long)tmp.base_uint(),(long long)bnTarget.base_uint());
+                fprintf(stderr,"%u: hash %llx > %llx target\n",nonce,tmp,(long long)bnTarget);
                 continue;
             }
             break;
