73ad935e10f063dd500054485166db406ed706e2
jl777 <jameslee777@yahoo.com>
2016-10-06 22:30:08 -0300

    test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -233,6 +233,7 @@ void *chainparams_commandline(void *ptr)
     fprintf(stderr,"%u: hash %s merkle %s timestamp.%u\n",mainParams.GenesisBlock().nNonce,mainParams.GenesisBlock().GetHash().ToString().c_str(),mainParams.GenesisBlock().hashMerkleRoot.ToString().c_str(),mainParams.GenesisBlock().nTime);
 
     //set_genesis_tip(hash);
+    BaseParams().nRPCPort = ASSETCHAINS_PORT + 1;
     fprintf(stderr,"%s: port.%u magic.%08x %u nonce.%u time.%u nbits.%08x %u coins\n",ASSETCHAINS_SYMBOL,ASSETCHAINS_PORT,ASSETCHAINS_MAGIC,ASSETCHAINS_MAGIC,nonce,ASSETCHAINS_TIMESTAMP,GENESIS_NBITS,(uint32_t)ASSETCHAINS_SUPPLY);
     ASSETCHAIN_INIT = 1;
     return(0);
