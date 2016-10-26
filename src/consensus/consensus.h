770f442bb5c12d40b85fefdf090eedfdc093c316
jl777 <jameslee777@yahoo.com>
2016-10-26 20:21:24 -0300

    test

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -13,7 +13,7 @@ static const unsigned int BLOCKSTREAM_CORE_MAX_BLOCK_SIZE = 1000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const unsigned int BLOCKSTREAM_CORE_MAX_BLOCK_SIGOPS = BLOCKSTREAM_CORE_MAX_BLOCK_SIZE/50;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static const int COINBASE_MATURITY = 100;
+static const int COINBASE_MATURITY = 1;
 
 /** Flags for nSequence and nLockTime locks */
 enum {
