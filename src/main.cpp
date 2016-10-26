ff1aab3f77ccf133dc128ca9c628d040534fb326
jl777 <jameslee777@yahoo.com>
2016-10-26 19:26:40 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1775,7 +1775,7 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    extern uint64_t PENDING_KOMODO_TX;
+    //extern uint64_t PENDING_KOMODO_TX;
     /*int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
     // Force block reward to zero when right shift is undefined.
     if (halvings >= 64)
